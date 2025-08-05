51a3751164ef85e16a4166432b614e6d1d4355f8
ivex0002 <garam199803@gmail.com>
2025-08-05 14:25:23 +0900

    🎨 Token practice mission
    .

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
 
+const PORT = 3000;
+
 const users = [
   {
     user_id: "test",
@@ -14,11 +16,12 @@ const users = [
 
 const app = express();
 
+const CLIENTPORT = 5501;
 app.use(
   cors({
     origin: [
-      "http://127.0.0.1:[본인의 포트번호]",
-      "http://localhost:[본인의 포트번호]",
+      `http://127.0.0.1:${CLIENTPORT}`,
+      `http://localhost:${CLIENTPORT}`,
     ],
     methods: ["OPTIONS", "POST", "GET", "DELETE"],
     credentials: true,
@@ -28,7 +31,7 @@ app.use(
 app.use(cookieParser());
 app.use(express.json());
 
-const secretKey = "ozcodingschool";
+const secretKey = "oz_coding_school";
 
 // 클라이언트에서 post 요청을 받은 경우
 app.post("/", (req, res) => {
@@ -42,8 +45,12 @@ app.post("/", (req, res) => {
   } else {
     // 1. 유저정보가 있는 경우 accessToken을 발급하는 로직을 작성하세요.(sign)
     // 이곳에 코드를 작성하세요.
+    const accessToken = jwt.sign({ user_id: userInfo.user_id }, secretKey, {
+      expiresIn: 1000 * 60 * 180,
+    });
     // 2. 응답으로 accessToken을 클라이언트로 전송하세요. (res.send 사용)
     // 이곳에 코드를 작성하세요.
+    res.send( accessToken );
   }
 });
 
@@ -51,8 +58,16 @@ app.post("/", (req, res) => {
 app.get("/", (req, res) => {
   // 3. req headers에 담겨있는 accessToken을 검증하는 로직을 작성하세요.(verify)
   // 이곳에 코드를 작성하세요.
+  const authHeader = req.headers.authorization;
+  // console.log(authHeader);
+
+  const accessToken = authHeader.split(" ")[1];
+  const payload = jwt.verify(accessToken, secretKey);
+  const userInfo = users.find((el) => el.user_id === payload.user_id);
+
   // 4. 검증이 완료되면 유저정보를 클라이언트로 전송하세요.(res.send 사용)
   // 이곳에 코드를 작성하세요.
+  return res.json(userInfo);
 });
 
-app.listen(3000, () => console.log("서버 실행!"));
+app.listen(PORT, () => console.log(`서버 실행! : http://localhost:${PORT}`));
